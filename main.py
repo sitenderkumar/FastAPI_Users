d3ec35a37aa5b0022d311bf95e45449f25878370
sitenderkumar <44888274+sitenderkumar@users.noreply.github.com>
2023-06-18 10:50:10 +0800

    Update main.py
    Changed status message from "OK" to "Success"

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ auth_token = HTTPBearer()
 @app.get("/getuser")
 def getuser():
     users = users_serializer(collection.find())
-    return {"status": "Ok","data": users}
+    return {"status": "Success","data": users}
 
 @app.get("/getuser/{id}")
 def getuser(id: int):
@@ -19,7 +19,7 @@ def getuser(id: int):
         user = user_serializer(collection.find_one({"id": id}))
     except:
         return {"status": 404, "data": "User not found"}
-    return {"status": "Ok","data": user}
+    return {"status": "Success","data": user}
 
 @app.post("/createuser", status_code=201)
 def createuser(user: User, token: str = Depends(auth_token)):
@@ -32,7 +32,7 @@ def createuser(user: User, token: str = Depends(auth_token)):
         return {"status": 400, "data": "User ID already exists"}
     _id = collection.insert_one(dict(user))
     user = users_serializer(collection.find({"_id": _id.inserted_id}))
-    return {"status": "Ok","data": user}
+    return {"status": "Success","data": user}
 
 @app.put("/updateuser/{id}")
 def updateuser(id: int, user: User, token: str = Depends(auth_token), ):
@@ -47,7 +47,7 @@ def updateuser(id: int, user: User, token: str = Depends(auth_token), ):
     if not oldDoc: 
         return {"status": 404, "data": "User not found"}
     user = user_serializer(collection.find_one({"id": user.id}))
-    return {"status": "Ok","data": user}
+    return {"status": "Success","data": user}
 
 @app.delete("/deleteuser/{id}")
 def deleteuser(id: int, token: str = Depends(auth_token), ):
@@ -60,4 +60,4 @@ def deleteuser(id: int, token: str = Depends(auth_token), ):
     )
     if not deleted: 
         return {"status": 404, "data": "User not found"}
-    return {"status": "Ok","data": []}
+    return {"status": "Success","data": []}
